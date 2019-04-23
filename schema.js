86fb0d5e877c483b3f29b956799cb992974ce7e1
unknown <devethancraig@gmail.com>
2019-04-23 14:02:13 -0600

    Created mock DB - Sends the Requestsvia Axios

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -6,12 +7,6 @@ const {
     GraphQLList,
     GraphQLNonNull
 } = require('graphql');
-
-const customers = [
-    { id: '1', name: 'Jon Doe', email: 'jond@gmail.com', age: 35 },
-    { id: '2', name: 'Jane Doe', email: 'janed@gmail.com', age: 32 },
-    { id: '3', name: 'Jeff Doe', email: 'jeffd@gmail.com', age: 29 }
-];
 
 const CustomerType = new GraphQLObjectType({
     name: 'Customer',
@@ -32,17 +27,20 @@ const RootQuery = new GraphQLObjectType({
                 id: { type: GraphQLString }
             },
             resolve(parentValue, args) {
-                for (let i = 0; i < customers.length; i++) {
-                    if (customers[i].id == args.id) {
-                        return customers[i];
-                    }
-                }
+                // for (let i = 0; i < customers.length; i++) {
+                //     if (customers[i].id == args.id) {
+                //         return customers[i];
+                //     }
+                // }
+                return axios.get('http://localhost:3000/customers/'+args.id)
+                    .then(res => res.data);
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args) {
-                return customers;
+                return axios.get('http://localhost:3000/customers')
+                    .then(res => res.data);
             }
         }
     }
