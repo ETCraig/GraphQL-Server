02b9f647aedc7cab3af7675c20ff51ed551b64bf
unknown <devethancraig@gmail.com>
2019-04-23 13:51:09 -0600

    Created Single && Group Calls

--- schema.js
+++ schema.js
@@ -1,12 +1,53 @@
 const {
     GraphQLObjectType,
-    graphQLString,
+    GraphQLString,
     GraphQLInt,
     GraphQLSchema,
     GraphQLList,
     GraphQLNonNull
 } = require('graphql');
 
+const customers = [
+    { id: '1', name: 'Jon Doe', email: 'jond@gmail.com', age: 35 },
+    { id: '2', name: 'Jane Doe', email: 'janed@gmail.com', age: 32 },
+    { id: '3', name: 'Jeff Doe', email: 'jeffd@gmail.com', age: 29 }
+];
+
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                for (let i = 0; i < customers.length; i++) {
+                    if (customers[i].id == args.id) {
+                        return customers[i];
+                    }
+                }
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+                return customers;
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
-    
+    query: RootQuery
 });
